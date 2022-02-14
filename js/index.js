d2ae3f88831702c308adc3aa0a9c004980cbbbf4
show5 <show5116@naver.com>
2022-02-14 15:31:13 +0900

    update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-const elm = document.querySelectorAll('section');
-const elmCount = elm.length;
 const nav = document.querySelector('nav');
 const skills = document.querySelector('#skill');
 const projects = document.querySelector('#project');
@@ -8,46 +6,7 @@ const carrers = document.querySelector('#career');
 slider(skills);
 slider(projects);
 slider(carrers);
-
-elm.forEach(function(item, index){
-    item.addEventListener('mousewheel', function(event){
-        event.preventDefault();
-        let delta = 0;
-
-        if (!event) event = window.event;
-        if (event.wheelDelta) {
-            delta = event.wheelDelta / 120;
-            if (window.opera) delta = -delta;
-        }
-        else if (event.detail)
-            delta = -event.detail / 3;
-
-        let moveTop = window.scrollY;
-        let elmSelector = elm[index];
-
-        // wheel down : move to next section
-        if (delta < 0){
-            if (index !== elmCount-1){
-                try{
-                    moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
-                }catch(e){}
-            }
-        }
-
-        // wheel up : move to previous section
-        else{
-            if (index !== 0){
-                try{
-                    moveTop = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
-                }catch(e){}
-            }
-        }
-
-        const body = document.querySelector('html');
-        window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
-    });
-});
-
+/*
 window.addEventListener("scroll",e=>{
     if(window.scrollY < 100){
         if (nav.classList.contains('appear')) {
@@ -63,4 +22,4 @@ window.addEventListener("scroll",e=>{
         nav.classList.remove("disappear");
         nav.classList.add("appear");
     }
-})
+})*/
